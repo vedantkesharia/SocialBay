7b22cfd7b2d7e22d7b61d82e56ba28d7e73fcf34
vedantkesharia <keshariavedant@gmail.com>
2023-06-19 20:45:06 +0530

    second

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,41 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper position="sticky" top="0">
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/info4.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>MikaCosmetics</Typography>
+        <Typography color={medium}>mikacosmetics.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Your pathway to stunning and immaculate beauty and made sure your skin
+        is exfoliating skin and shining like light.
+        Several testers remarked that it was gentle enough for sensitive skin, and it's great for all skin types. 
+        Testers ranked it highest of all cleansers tested. Plus, it scored well for rinsing off easily and making skin feel hydrated.
+        This product has been awarded the best skin product of the year!!!
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
