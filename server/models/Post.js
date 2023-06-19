96db96fb4b932833e229da4d07c6fa8b71787d7d
vedantkesharia <keshariavedant@gmail.com>
2023-06-19 19:16:39 +0530

    initial commit

---
+++ server/models/Post.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location:{type: String},
+    description:{type: String},
+    picturePath:{type: [Array,String]},
+    userPicturePath: {type: [Array,String]},
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
